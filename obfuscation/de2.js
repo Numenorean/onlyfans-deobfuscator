d22b11e32c61f82f77ae0bdc20425e0df7942945
Numenorean <skillzq@yahoo.com>
2022-12-17 15:02:02 +0200

    initial

---
+++ obfuscation/de2.js
@@ -0,0 +1,24 @@
+"use strict";
+
+(self.webpackChunkof_vue = self.webpackChunkof_vue || []).push([
+  [3415], {
+    833415: function(n, W, t) {
+      var c = t(193810),
+        o = t.n(c),
+        r = t(227361),
+        u = t.n(r),
+        e = t(550615);
+      W.Z = n => {
+        const t = n.url || "";
+        window.navigator.userAgent || null;
+        const r = {};
+        r.time = +new Date();
+        const k = e.Z.getters["auth/authUserId"] || null,
+          i = o()(["sQ8l6MEiw7osKSq4vT2wdTzO3sWLyIEO", r.time, t, k || 0].join("\n"));
+        return r.sign = ["6205", i, function(n) {
+          return Math.abs(n[8606 % n.length].charCodeAt(0) - 107 + (n[7955 % n.length].charCodeAt(0) - 94) + (n[6426 % n.length].charCodeAt(0) - 99) + (n[8824 % n.length].charCodeAt(0) + 80) + (n[7261 % n.length].charCodeAt(0) + 129) + (n[8660 % n.length].charCodeAt(0) + 74) + (n[8070 % n.length].charCodeAt(0) - 121) + (n[6284 % n.length].charCodeAt(0) - 91) + (n[8905 % n.length].charCodeAt(0) - 90) + (n[6623 % n.length].charCodeAt(0) + 107) + (n[6710 % n.length].charCodeAt(0) - 109) + (n[7077 % n.length].charCodeAt(0) + 127) + (n[7161 % n.length].charCodeAt(0) + 109) + (n[6547 % n.length].charCodeAt(0) - 140) + (n[7797 % n.length].charCodeAt(0) - 134) + (n[7632 % n.length].charCodeAt(0) + 134) + (n[8181 % n.length].charCodeAt(0) - 132) + (n[8236 % n.length].charCodeAt(0) - 67) + (n[8355 % n.length].charCodeAt(0) - 122) + (n[6876 % n.length].charCodeAt(0) - 79) + (n[8768 % n.length].charCodeAt(0) + 108) + (n[7430 % n.length].charCodeAt(0) + 56) + (n[6353 % n.length].charCodeAt(0) - 86) + (n[8529 % n.length].charCodeAt(0) - 86) + (n[6955 % n.length].charCodeAt(0) - 90) + (n[9020 % n.length].charCodeAt(0) - 143) + (n[6825 % n.length].charCodeAt(0) - 124) + (n[7880 % n.length].charCodeAt(0) - 91) + (n[7514 % n.length].charCodeAt(0) + 110) + (n[7684 % n.length].charCodeAt(0) + 56) + (n[8460 % n.length].charCodeAt(0) - 117) + (n[7380 % n.length].charCodeAt(0) - 139)).toString(16);
+        }(i), "639995f4"].join(":"), r;
+      };
+    }
+  }
+]);
