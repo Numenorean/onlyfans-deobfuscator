a54b88c07d63a2be1d25562b33abe0fbd82a42a4
Numenorean <skillzq@yahoo.com>
2023-02-28 15:17:39 +0200

    better naming

--- src/checksum_finder.ts
+++ src/checksum_finder.ts
@@ -4,7 +4,7 @@ import * as t from "@babel/types";
 import { readFileSync } from "fs";
 
 function findConsts(ast: parser.ParseResult<t.File>, hashLength: number = 40): [number[], number[]] {
-    const checksums: number[] = [];
+    const consts: number[] = [];
     const indexes: number[] = [];
     
     traverse(ast, {
@@ -13,9 +13,9 @@ function findConsts(ast: parser.ParseResult<t.File>, hashLength: number = 40): [
             if (t.isNumericLiteral(node.right)) {
                 const right = node.right as t.NumericLiteral;
                 if (node.operator === '+') {
-                    checksums.push(right.value);
+                    consts.push(right.value);
                 } else if (node.operator === '-') {
-                    checksums.push(-right.value);
+                    consts.push(-right.value);
                 } else {
                     console.error("Unhandler operator: ", node.operator);
                 }
@@ -26,12 +26,12 @@ function findConsts(ast: parser.ParseResult<t.File>, hashLength: number = 40): [
         }
     });
 
-    return [checksums, indexes];
+    return [consts, indexes];
 }
 
 const ast = parser.parse(readFileSync(process.argv[2], "utf8"));
-const [checksums, indexes] = findConsts(ast);
-const checksum = checksums.reduce((partialSum, a) => partialSum + a, 0);
-console.log(`Checksums: [${checksums}]
-Checksum: ${checksum}
+const [consts, indexes] = findConsts(ast);
+const total_const = consts.reduce((partialSum, a) => partialSum + a, 0);
+console.log(`Consts: [${consts}]
+Const: ${total_const}
 Indexes: [${indexes}]`);
